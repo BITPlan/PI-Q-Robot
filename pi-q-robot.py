fbdd56ed5a53701e2ea29d9372ee38f8ef6895be
Wolfgang Fahl <wf@bitplan.com>
2020-01-19 09:31:52 +0100

    improves angle handling and handles inversion

--- pi-q-robot.py
+++ pi-q-robot.py
@@ -30,44 +30,52 @@ app = Flask(__name__, static_url_path='', static_folder='web')
 api = Api(app)
 
 class Servo:
-    def __init__(self, id):
+    """ I am a single Servo motor """
+    def __init__(self, id,inverted=False):
         self.id = id
+        self.inverted=inverted
 
     def setAngle(self, angle):
         self.angle = angle
+        trueAngle=180-angle if self.inverted else angle
         if kit is not None:
-            kit.servo[self.id].angle = angle
+            kit.servo[self.id].angle = trueAngle
+        return trueAngle
 
 class Extremity:
-    def __init__(self, name, id):
+    """ I am a single extrimity of the spider  e.g. coxa, femur or tibia of one of the legs"""
+    def __init__(self, name, id,inverted=False):
         self.name = name
-        self.servo = Servo(id)
+        self.servo = Servo(id,inverted)
 
     def setAngle(self, angle):
         self.servo.setAngle(angle)
 
 class Leg:
-    def __init__(self, coxaId, tibiaId, femurId):
+    """ I am a single leg of the spider consisting of coxa, femur and tibia"""
+    def __init__(self, legId,coxaId, femurId, tibiaId):
+        self.legId=legId
         self.coxa = Extremity('coxa', coxaId)
-        self.tibia = Extremity('tibia', tibiaId)
-        self.femur = Extremity('femur', femurId)
+        self.femur = Extremity('femur', femurId,legId>=2)
+        self.tibia = Extremity('tibia', tibiaId,legId>=2)
 
     def stand(self):
-        self.coxa.setAngle(105)
-        self.tibia.setAngle(105)
-        self.femur.setAngle(105)
+        self.coxa.setAngle(90)
+        self.femur.setAngle(45)
+        self.tibia.setAngle(55)
 
     def sit(self):
-        self.coxa.setAngle(180)
-        self.tibia.setAngle(180)
-        self.femur.setAngle(180)
+        self.coxa.setAngle(90)
+        self.femur.setAngle(90)
+        self.tibia.setAngle(0)
 
 class Spider:
+    """ I am a quadruped spider with four legs consisting of coxa, femur and tibia each"""
     def __init__(self):
-        self.fl = Leg(0, 1, 2)
-        self.fr = Leg(3, 4, 5)
-        self.rl = Leg(6, 7, 8)
-        self.rr = Leg(9, 10, 11)
+        self.fl = Leg(0, 0,  1,  2)
+        self.fr = Leg(1, 3,  4,  5)
+        self.rl = Leg(2, 6,  7,  8)
+        self.rr = Leg(3, 9, 10, 11)
 
     def stand(self):
         self.fl.stand()
@@ -114,8 +122,8 @@ def setservo(servoId,angle):
     msg="setting servo %d to angle %d" % (servoId,angle)
     print (msg)
     servo = Servo(servoId)
-    servo.setAngle(angle)
-    json="{ angle: %d, id: %d}" % (angle,servoId)
+    trueAngle=servo.setAngle(angle)
+    json="{ id: %d, angle: %d, trueAngle: %d}" % (servoId,angle,trueAngle)
     return json
 
 @app.route("/action", methods=['POST'])
@@ -127,7 +135,7 @@ def action():
         spider.stand()
     elif action == 'sit':
         spider.sit()
-    return index(msg) 
+    return index(msg)
 
 if __name__ == '__main__':
     app.run(port='5002', host='0.0.0.0')
